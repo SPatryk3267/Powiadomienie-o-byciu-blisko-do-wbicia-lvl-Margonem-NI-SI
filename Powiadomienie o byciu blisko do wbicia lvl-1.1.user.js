d87b97203dad0ef2b287ef6e039b6a985bdcfaf3
SPatryk3267 <77066454+SPatryk3267@users.noreply.github.com>
2024-12-04 10:35:59 +0100

    Add files via upload

---
+++ Powiadomienie o byciu blisko do wbicia lvl-1.1.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Powiadomienie o byciu blisko do wbicia lvl
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Powiadomienie o byciu blisko do wbicia lvl
+// @author       SPatryk3267
+// @match        https://*.margonem.pl/
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+
+
+
+function checkExpPercent() {
+    const isInterfaceSi = getCookie("interface") === "si";
+
+    if ((!isInterfaceSi && Engine.hero?.d?.lvl && Engine.hero?.d?.exp) ||
+        (isInterfaceSi && hero?.exp && hero?.lvl)) {
+
+        const exp = isInterfaceSi ? hero.exp : Engine.hero.d.exp;
+        const lvl = isInterfaceSi ? hero.lvl : Engine.hero.d.lvl;
+
+        const expNext = lvl ** 4 + 10;
+        const expPrev = (lvl - 1) ** 4 + 10;
+        const percent = ((exp - expPrev) / (expNext - expPrev)) * 100;
+
+        if (percent >= 0) {
+            message(`Uwaga! Masz ${percent.toFixed(2)}% doświadczenia!`);
+        }
+    }
+
+    if (!isInterfaceSi) {
+
+        window.API.addCallbackToEvent("close_battle", checkExpPercent);
+    } else {
+
+        g.loadQueue.push({
+            fun: () => {
+                setTimeout(() => {
+                    checkExpPercent();
+                }, 15000);
+            }
+        });
+    }
+}
+
+setTimeout(() => {
+    checkExpPercent();
+}, 5000);
